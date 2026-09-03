a3f954170283e9c59a5f027867a9d628c32e654e
deguzman-margaux <deguzman.mre@obmontessori.edu.ph>
2026-09-04 07:51:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+studname = "Lance Burgos"
+studage = 15 #Integer
+stud_height = 162.5 #Floating-point
+stud_country_to_visit = ["Japan", "USA", "Canada"]
+stud_new = False #Boolean
+stud_info = {
+    "color" : "Blue", 
+    "car_brand" : "Ford", 
+    "shoe_size" : 10,
+    "bff" : "No One"
+    } #Dictionary
+stud_favefruits = {
+    "Orange", 
+    "Apple", 
+    "Watermellon", 
+    "Grapes", 
+    "Mango"
+    } #Set
+days_of_week = (
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
+    )
+
+print("Name:", studname)
+print("Age:", studage)
+print("Height:", stud_height)
+print("Counties to Visit:", stud_country_to_visit)
+print("New Student:", stud_new)
+print("Info:", stud_info)
